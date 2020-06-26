f6fa52947b2f40a8f0638a63e49ec76f98477a29
Thiago Pereira <thiagorpereira7@gmail.com>
2020-06-26 17:00:20 -0300

    Filter Points

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -4,6 +4,25 @@ import knex from '../database/connection';
 
 class PointsController {
 
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()))
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct() // Don't repeat points if have same Items
+      .select('points.*'); // Only data os points and not of point_items
+
+    // console.log(city, uf, items);
+    return res.json(points)
+  }
+
   async show(req: Request, res: Response) {
     const { id } = req.params;
 
@@ -18,7 +37,7 @@ class PointsController {
      *  JOIN point_items ON items.id = point_items.item.id
      * WHERE point_items.point_id = {id}
      */
-    
+
     // Items that have relation with id Point 
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
@@ -45,7 +64,7 @@ class PointsController {
     const trx = await knex.transaction();
     
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
@@ -68,7 +87,8 @@ class PointsController {
   
     await trx('point_items').insert(pointItems);
   
-  
+    await trx.commit();
+
     return res.json({
       id: point_id,
       ...point,
